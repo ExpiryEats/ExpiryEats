8a3d825bcbe30f0fde71bbf9ed5a04108621215b
anmolrai853 <annolrai853@gmail.com>
2025-03-03 22:40:50 +0000

    login/register

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,95 @@
+import 'package:expiry_eats/colors.dart';
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'register_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController _usernameController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+
+    void _login() {
+      final String username = _usernameController.text;
+      final String password = _passwordController.text;
+
+      if (username.isEmpty || password.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Please enter both username and password')),
+        );
+      } else {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+        );
+      }
+    }
+
+    void _register() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const RegisterScreen()),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expiry Eats'),
+      ),
+      body: Container(
+        color: AppTheme.primary80,
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Container(
+            width: 400, 
+            height: 500, 
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                  Image.asset('lib/assets/ExpiryLogo.png', height: 160),
+                const Text(
+                  'Expiry Eats',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 32),
+                TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  onPressed: _login,
+                  child: const Text('Login'),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: _register,
+                  child: const Text('Register'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
