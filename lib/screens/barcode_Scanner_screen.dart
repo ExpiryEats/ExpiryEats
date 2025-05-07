ec0b0bdcefabd882a3ba945b7861301ad8923382
Bruno Mendes <up2247056@myport.ac.uk>
2025-05-07 15:06:51 +0100

    Create barcode_Scanner_screen.dart

---
+++ lib/screens/barcode_Scanner_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+
+class BarcodeScannerScreen extends StatefulWidget {
+  const BarcodeScannerScreen({super.key});
+
+  @override
+  State<BarcodeScannerScreen> createState() => _BarcodeScannerScreenState();
+}
+
+class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
+  late MobileScannerController cameraController;
+  String? _scannedBarcode;
+  bool _torch = false;
+
+
+  @override
+  void initState () {
+    super.initState();
+    cameraController = MobileScannerController(
+      torchEnabled: _torch,
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scan Barcode'),
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: _torch
+              ? const Icon(Icons.flash_on, color: Colors.yellow)
+              : const Icon(Icons.flash_off, color: Colors.grey),
+            onPressed: () {
+              setState(() {
+                _torch = !_torch;
+              });
+              cameraController.toggleTorch();
+            }
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: MobileScanner(
+              controller: cameraController,
+              onDetect: (BarcodeCapture capture) {
+                final List<Barcode> barcodes = capture.barcodes;
+                if (barcodes.isNotEmpty) {
+                  final String? barcode = barcodes.first.rawValue;
+                  if (barcode != null && mounted) {
+                    Navigator.pop(context, barcode);
+                  }
+                }
+              },
+            ),
+          ),
+          if (_scannedBarcode != null)
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text('Scanned code: $_scannedBarcode',
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  @override
+  void dispose() {
+    cameraController.dispose();
+    super.dispose();
+  }
+}
